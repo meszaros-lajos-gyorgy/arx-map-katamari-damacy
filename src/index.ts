0d9678201edbe1aec2adc82ca4debfd2c234585f
Lajos Meszaros <m_lajos@hotmail.com>
2024-10-27 16:31:15 +0100

    refactor: move hud display functionality to a separate entity

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { ArxMap, Entity, HudElements, QUADIFY, Settings, SHADING_SMOOTH, Texture, Vector3 } from 'arx-level-generator'
-import { createBox, createPlaneMesh } from 'arx-level-generator/prefabs/mesh'
+import { ArxMap, HudElements, QUADIFY, Settings, SHADING_SMOOTH, Vector3 } from 'arx-level-generator'
+import { createPlaneMesh } from 'arx-level-generator/prefabs/mesh'
 import { ScriptSubroutine } from 'arx-level-generator/scripting'
-import { useDelay } from 'arx-level-generator/scripting/hooks'
-import { PlayerControls, Shadow, Speed, Variable } from 'arx-level-generator/scripting/properties'
+import { Shadow, Speed, Variable } from 'arx-level-generator/scripting/properties'
 import { createLight } from 'arx-level-generator/tools'
 import { applyTransformations, isBetween } from 'arx-level-generator/utils'
 import { randomBetween } from 'arx-level-generator/utils/random'
 import { Mesh } from 'three'
+import { createGameState } from './prefabs/gamestate.js'
 import { createNpc, createRootNpc, NpcTypes } from './prefabs/npc.js'
 
 const settings = new Settings()
@@ -27,10 +27,9 @@ meshes.push(plane)
 
 // -----------------------
 
-const hudLine1 = new Variable('string', 'hud_line_1', ' ')
-const hudLine2 = new Variable('string', 'hud_line_2', ' ')
-const hudLine3 = new Variable('string', 'hud_line_3', ' ')
-const hudLine4 = new Variable('string', 'hud_line_4', ' ')
+const gameState = createGameState()
+map.entities.push(gameState)
+
 const size = new Variable('float', 'size', 50) // real height of the model (centimeters)
 const baseHeight = new Variable('int', 'base_height', 180) // model height (centimeters)
 const scaleFactor = new Variable('float', 'scale_factor', 0, true) // value to be passed to setscale command (percentage)
@@ -38,23 +37,11 @@ const tmp = new Variable('float', 'tmp', 0, true) // helper for calculations
 
 map.player.withScript()
 
-map.player.script?.properties.push(
-  new Speed(1.5),
-  Shadow.off,
-  hudLine1,
-  hudLine2,
-  hudLine3,
-  hudLine4,
-  size,
-  baseHeight,
-  scaleFactor,
-  tmp,
-)
+map.player.script?.properties.push(new Speed(1.5), Shadow.off, size, baseHeight, scaleFactor, tmp)
 
 const resize = new ScriptSubroutine(
   'resize',
   () => {
-    const { delay } = useDelay()
     return `
 // scaleFactor % = (playerSize cm / playerBaseHeight cm) * 100
 set ${scaleFactor.name} ${size.name}
@@ -62,28 +49,7 @@ div ${scaleFactor.name} ${baseHeight.name}
 mul ${scaleFactor.name} 100
 
 setscale ${scaleFactor.name}
-${delay(500)} sendevent -g consumables size_threshold_change ~${size.name}~
-
-// display scale rounded to 2 decimals
-
-// wholepart = (int)size
-set §wholepart ${size.name}
-
-// decimaldigit1 = (int)((size - wholepart) * 10)
-set ${tmp.name} ${size.name}
-dec ${tmp.name} §wholepart
-mul ${tmp.name} 10
-set §decimaldigit1 ${tmp.name}
-
-// decimaldigit2 = (int)(((size - wholepart) * 10 - decimaldigit1) * 10)
-set ${tmp.name} ${size.name}
-dec ${tmp.name} §wholepart
-mul ${tmp.name} 10
-dec ${tmp.name} §decimaldigit1
-mul ${tmp.name} 10
-set §decimaldigit2 ${tmp.name}
-
-set ${hudLine1.name} "player size: ~§wholepart~.~§decimaldigit1~~§decimaldigit2~cm"
+sendevent player_resized ${gameState.ref} ~${size.name}~
 `
   },
   'gosub',
@@ -91,7 +57,7 @@ set ${hudLine1.name} "player size: ~§wholepart~.~§decimaldigit1~~§decimaldigi
 map.player.script?.subroutines.push(resize)
 
 map.player.script
-  ?.on('init', () => {
+  ?.on('initend', () => {
     return `
 ${resize.invoke()}
     `
@@ -105,20 +71,6 @@ inc ${size.name} ${tmp.name}
 
 ${resize.invoke()}
 `
-  })
-  .on('main', () => {
-    return `
-herosay ${hudLine1.name}
-herosay ${hudLine2.name}
-herosay ${hudLine3.name}
-herosay ${hudLine4.name}
-    `
-  })
-  .on('victory', () => {
-    return `
-set ${hudLine2.name} "You've won!!"
-${PlayerControls.off}
-    `
   })
 
 map.hud.hide(HudElements.HerosayIcon)
@@ -199,7 +151,7 @@ const boss = createNpc({
   type: NpcTypes.GoblinKing,
 })
 boss.script?.on('consumed', () => {
-  return `sendevent victory player nop`
+  return `sendevent victory ${gameState.ref} nop`
 })
 map.entities.push(boss)
 
---
+++ src/prefabs/gamestate.ts
@@ -0,0 +1,74 @@
+import { Entity } from 'arx-level-generator'
+import { ScriptSubroutine } from 'arx-level-generator/scripting'
+import { useDelay } from 'arx-level-generator/scripting/hooks'
+import { PlayerControls, Variable } from 'arx-level-generator/scripting/properties'
+
+export function createGameState() {
+  const gameState = Entity.marker.withScript()
+
+  const hudLine1 = new Variable('string', 'hud_line_1', ' ')
+  const hudLine2 = new Variable('string', 'hud_line_2', ' ')
+  const hudLine3 = new Variable('string', 'hud_line_3', ' ')
+  const hudLine4 = new Variable('string', 'hud_line_4', ' ')
+  const tmp = new Variable('float', 'tmp', 0, true) // helper for calculations
+
+  gameState.script?.properties.push(hudLine1, hudLine2, hudLine3, hudLine4, tmp)
+
+  const redraw = new ScriptSubroutine('redraw', () => {
+    return `
+herosay ${hudLine1.name}
+herosay ${hudLine2.name}
+herosay ${hudLine3.name}
+herosay ${hudLine4.name}
+    `
+  })
+
+  gameState.script?.subroutines.push(redraw)
+
+  gameState.script
+    ?.on('initend', () => {
+      const { loop } = useDelay()
+      return `
+// setmainevent and the main event in general doesn't seem to be working for markers
+// https://wiki.arx-libertatis.org/Script:setmainevent
+${redraw.invoke()}
+${loop(1000)} ${redraw.invoke()}
+      `
+    })
+    .on('player_resized', () => {
+      return `
+sendevent -g consumables size_threshold_change ~^&param1~
+
+// display scale rounded to 2 decimals
+
+// wholepart = (int)size
+set §wholepart ^&param1
+
+// decimaldigit1 = (int)((size - wholepart) * 10)
+set ${tmp.name} ^&param1
+dec ${tmp.name} §wholepart
+mul ${tmp.name} 10
+set §decimaldigit1 ${tmp.name}
+
+// decimaldigit2 = (int)(((size - wholepart) * 10 - decimaldigit1) * 10)
+set ${tmp.name} ^&param1
+dec ${tmp.name} §wholepart
+mul ${tmp.name} 10
+dec ${tmp.name} §decimaldigit1
+mul ${tmp.name} 10
+set §decimaldigit2 ${tmp.name}
+
+set ${hudLine1.name} "player size: ~§wholepart~.~§decimaldigit1~~§decimaldigit2~cm"
+
+${redraw.invoke()}
+      `
+    })
+    .on('victory', () => {
+      return `
+set ${hudLine2.name} "You've won!!"
+${PlayerControls.off}
+      `
+    })
+
+  return gameState
+}
