c603e47df02b8a129b502e86bb2832d87aed1f94
Lajos Meszaros <m_lajos@hotmail.com>
2024-11-03 15:01:59 +0100

    feat: add randomized textures to the floor

--- src/places/levels/01/level01.ts
+++ src/places/levels/01/level01.ts
@@ -1,7 +1,18 @@
 import { ArxMap, QUADIFY, SHADING_SMOOTH, Vector3 } from 'arx-level-generator'
 import { createPlaneMesh } from 'arx-level-generator/prefabs/mesh'
 import { createLight } from 'arx-level-generator/tools'
-import { randomBetween } from 'arx-level-generator/utils/random'
+import { pickRandom, pickWeightedRandoms, randomBetween } from 'arx-level-generator/utils/random'
+import { Vector2 } from 'three'
+import {
+  soilHumanSoil1,
+  soilHumanStandard1,
+  soilHumanStandard2,
+  soilHumanStandard3,
+  stoneHumanCityGround1,
+  stoneHumanCityGround2,
+  stoneHumanCityGround3,
+  stoneHumanCityGround4,
+} from '@/textures.js'
 
 export function createLevel01(): ArxMap {
   const map = new ArxMap()
@@ -9,12 +20,37 @@ export function createLevel01(): ArxMap {
   map.polygons.addThreeJsMesh(
     createPlaneMesh({
       size: 4000,
+      tileSize: 50,
     }),
     {
       tryToQuadify: QUADIFY,
       shading: SHADING_SMOOTH,
     },
   )
+
+  const tiles = pickWeightedRandoms(map.polygons.length, [
+    { value: soilHumanSoil1, weight: 30 },
+    { value: soilHumanStandard1, weight: 10 },
+    { value: soilHumanStandard2, weight: 15 },
+    { value: soilHumanStandard3, weight: 15 },
+    { value: stoneHumanCityGround1, weight: 1 },
+    { value: stoneHumanCityGround2, weight: 20 },
+    { value: stoneHumanCityGround3, weight: 20 },
+    { value: stoneHumanCityGround4, weight: 15 },
+  ])
+
+  const uvOffsets = [new Vector2(0, 0), new Vector2(0, 0.5), new Vector2(0.5, 0), new Vector2(0.5, 0.5)]
+
+  map.polygons.forEach((polygon, idx) => {
+    const texture = tiles[idx].value
+    polygon.texture = texture
+    if (texture === soilHumanSoil1) {
+      const uvOffset = pickRandom(uvOffsets)
+      polygon.vertices.forEach((vertex) => {
+        vertex.uv.divideScalar(2).add(uvOffset)
+      })
+    }
+  })
 
   for (let x = 0; x < 8; x++) {
     for (let z = 0; z < 8; z++) {
---
+++ src/textures.ts
@@ -0,0 +1,44 @@
+import { Texture } from 'arx-level-generator'
+
+export const soilHumanSoil1 = new Texture({
+  filename: '[soil]_human_soil1.jpg',
+  isNative: true,
+  size: 256,
+})
+
+export const soilHumanStandard1 = new Texture({
+  filename: '[soil]_human_standard1.jpg',
+  isNative: true,
+  size: 128,
+})
+export const soilHumanStandard2 = new Texture({
+  filename: '[soil]_human_standard2.jpg',
+  isNative: true,
+  size: 128,
+})
+export const soilHumanStandard3 = new Texture({
+  filename: '[soil]_human_standard3.jpg',
+  isNative: true,
+  size: 128,
+})
+
+export const stoneHumanCityGround1 = new Texture({
+  filename: '[stone]_human_city_ground1.jpg',
+  isNative: true,
+  size: 128,
+})
+export const stoneHumanCityGround2 = new Texture({
+  filename: '[stone]_human_city_ground2.jpg',
+  isNative: true,
+  size: 128,
+})
+export const stoneHumanCityGround3 = new Texture({
+  filename: '[stone]_human_city_ground3.jpg',
+  isNative: true,
+  size: 128,
+})
+export const stoneHumanCityGround4 = new Texture({
+  filename: '[stone]_human_city_ground4.jpg',
+  isNative: true,
+  size: 128,
+})
